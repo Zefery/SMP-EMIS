aec3a4c0fc44eb2076ef643640ae5f479196558f
Zefery <135243879+Zefery@users.noreply.github.com>
2024-08-13 18:26:52 +0700

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,25 @@
+print("Hello", end=" ")
+print("word")
+
+First_name = "andi lorem ipsum bls blabla"
+name = "johan"
+
+full_name = First_name+" "+name
+
+print(First_name)
+print("welcome " + name + "!" )
+print("welcome", name ,"!" )
+print(full_name)
+
+name1 = "yusuf"
+print(name1)
+print(name)
+
+
+cita_cita = input("tolong masukkan cita-cita anda :" )
+print (cita_cita)
+
+buah = [jeruk, apel]
+
+print(buah)
+
