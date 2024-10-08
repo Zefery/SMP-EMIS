23ef5e19c17ae32e7d3defea749438041e1bf4bd
dafi21139 <muhammaddafichalifah@gmail.com>
2024-10-08 18:15:18 +0700

    python
    python

---
+++ main.py
@@ -0,0 +1,17 @@
+nama = input("masukan nama anda : ")
+
+email = input ("masukan email anda:")
+
+nomer_hp = input ("masukan nomer hp anda")
+
+print ("nama anda ",nama,"!")
+
+print ("email anda ",email,"!")
+
+print ("nomer hp anda ",nomer_hp,"!")
+
+print("HI",nama,"!")
+
+print("halo ",email,"!")
+
+print("ok",nomer_hp,"!")
